7de2c86fd96cadbb49f9d87e33b9d768d33d2d5b
Vapor-Trail <zucnick@gmail.com>
2016-07-28 17:12:48 -0700

    Update RPC.py
    Gives optional option to stop playing!

--- RPC.py
+++ RPC.py
@@ -3,30 +3,37 @@ def checkInput(selection):
     print "Player made invalidselection.Exiting"
     exit()
 
-player1 = raw_input("Player 1 Picks: ")
-player2 = raw_input("Player 2 Picks: ")
+keepPlaying = "yes"
+while keepPlaying == "yes":
 
-checkInput(player1)
-checkInput(player2)
+  player1 = raw_input("Player 1 Picks: ")
+  player2 = raw_input("Player 2 Picks: ")
 
-print player1
+  checkInput(player1)
+  checkInput(player2)
 
-print player2
-if player1 == player2:
-  print "It's a tie!"
-if player1 == "rock":
-  if player2 == "paper":
-     print "Player 2 wins"
+  print player1
+  print player2
+  
+  if player1 == player2:
+    print "It's a tie!"
+ 
+  if player1 == "rock":
+    if player2 == "paper":
+       print "Player 2 wins"
   if player2  == "scissors":
       print "Player 1 wins"
-if  player1 == "paper":
-  if player2 == "scissors":
-    print "Player 2 wins"
-  if player2 == "rock":
-    print "Player 1 wins"
+ 
+  if  player1 == "paper":
+    if player2 == "scissors":
+      print "Player 2 wins"
+    if player2 == "rock":
+      print "Player 1 wins"
     
-if player1 == "scissors":
-  if player2 == "papper":
-    print "Player 1 wins"
-  if player2 == "rock":
-    print "Player 2 wins"
+  if player1 == "scissors":
+    if player2 == "papper":
+      print "Player 1 wins"
+    if player2 == "rock":
+      print "Player 2 wins"
+  keepPlaying =raw_input("Do you want to keep playing? (yes/no)")
+  print "Game Over!"
